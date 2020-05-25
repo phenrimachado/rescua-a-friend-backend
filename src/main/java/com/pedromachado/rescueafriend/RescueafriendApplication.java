7ec4da96ef64f022823ce4d77b57642f40572236
Pedro Machado <pedrohmf2610@hotmail.com>
2020-05-25 17:31:57 -0300

    [Rescue a Friend] - Projeto criado

---
+++ src/main/java/com/pedromachado/rescueafriend/RescueafriendApplication.java
@@ -0,0 +1,13 @@
+package com.pedromachado.rescueafriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueafriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescueafriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedromachado/rescueafriend/RescueafriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromachado.rescueafriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueafriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
